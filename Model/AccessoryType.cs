4ad242c2f2c28a517a75353b5552fd621ff22378
ANDY840119-PC\andy840119 <andy840119@gmail.com>
2018-03-02 02:19:29 +0900

    add class

---
+++ Model/AccessoryType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NendoroidAccessorySearchEngine.Core.Model
+{
+    /// <summary>
+    /// accessory type
+    /// </summary>
+    public class AccessoryType
+    {
+
+    }
+}
---
+++ Model/SearchResult.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NendoroidAccessorySearchEngine.Core.Model
+{
+    /// <summary>
+    /// search result
+    /// </summary>
+    public class SearchResult
+    {
+        /// <summary>
+        /// Number of Nendoroid
+        /// </summary>
+        public string No { get; set; }
+
+        /// <summary>
+        /// display name
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Display image
+        /// </summary>
+        public string DisplayImage { get; set; }
+
+        /// <summary>
+        /// accessory type
+        /// </summary>
+        public AccessoryType AccessoryType { get; set; }
+
+        /// <summary>
+        /// introduce url
+        /// </summary>
+        public string IntroduceUrl { get; set; }
+    }
+}
