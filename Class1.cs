9cad74b948ac1576186397a3304e7c866d66a201
andy840119 <andy840119@gmail.com>
2018-03-02 16:21:29 +0800

    加上一些class

--- Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace NendoroidAccessorySearchEngine.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ SearchEngine.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NendoroidAccessorySearchEngine.Core.Model;
+using Nendoroido.Core.Import;
+
+namespace NendoroidAccessorySearchEngine.Core
+{
+    /// <summary>
+    /// search engine
+    /// </summary>
+    public class SearchEngine
+    {
+        /// <summary>
+        /// Importer
+        /// </summary>
+        public BaseInporter Importer { get; set; }
+
+
+
+        /// <summary>
+        /// call import,
+        /// will import the source file to SearchEngine for analyze
+        /// </summary>
+        public void Import()
+        {
+
+        }
+
+        //throw the image into here, and get the result
+        public async Task<List<SearchResult>> Search()
+        {
+            return null;
+        }
+    }
+}
