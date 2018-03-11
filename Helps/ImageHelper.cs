e1927c0173cb4c621339c301e1610a353475ad32
ANDY840119-PC\andy840119 <andy840119@gmail.com>
2018-03-11 17:47:25 +0900

    新增Helper 和測試相關物件

---
+++ Helps/ImageHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace Nendoroido.Core.Helps
+{
+    class ImageHelper
+    {
+        /// <summary>
+        /// Convert from byte to image
+        /// https://stackoverflow.com/questions/29153967/convert-a-byte-into-an-emgu-opencv-image
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <returns></returns>
+        public static Image<Gray, byte> ConvertFromByte(byte[] bytes, int width, int height)
+        {
+            Image<Gray, byte> depthImage = new Image<Gray, byte>(width, height);
+            depthImage.Bytes = bytes;
+            return depthImage;
+        }
+    }
+}
